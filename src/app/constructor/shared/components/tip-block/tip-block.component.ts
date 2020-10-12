a2c4805b144d7ce875146c81b81e3b7603dd30b1
Bohdan <bogdanmuha5@gmail.com>
2020-10-12 21:09:51 +0300

    redesign blocks

--- src/app/constructor/shared/components/tip-block/tip-block.component.ts
+++ src/app/constructor/shared/components/tip-block/tip-block.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import Quill from 'quill';
 
 @Component({
   selector: 'app-tip-block',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TipBlockComponent implements OnInit {
 
+  editorStyle = {
+    height: 'auto',
+    background: '#FEE983',
+    border: '1px solid #F4F4F4',
+    boxSizing: 'border-box',
+    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)'
+  };
+
+  modules = {
+    toolbar: [
+      [{ size:  ['small', false, 'large', 'huge'] }],
+      ['bold', 'italic', 'underline'],
+      [{ align: null}, {align: 'center'}, {align: 'right'}, {align: 'justify'}],
+    ]
+  };
+
+  tipBlock = new FormControl('');
+
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
