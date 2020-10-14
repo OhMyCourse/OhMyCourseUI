af4786267e513ec36a50a846700dc5913bd5f790
Bohdan <bogdanmuha5@gmail.com>
2020-10-14 12:32:19 +0300

    add video block

--- src/app/constructor/blocks-menu/blocks-menu.component.ts
+++ src/app/constructor/blocks-menu/blocks-menu.component.ts
@@ -3,6 +3,7 @@ import { Block } from '../shared/models/Block';
 import { TextBlock } from '../shared/models/TextBlock';
 import { ImageBlock } from '../shared/models/ImageBlock';
 import { TipBlock } from '../shared/models/TipBlock';
+import { VideoBlock } from '../shared/models/VideoBlock';
 
 @Component({
   selector: 'app-blocks-menu',
@@ -14,7 +15,7 @@ export class BlocksMenuComponent implements OnInit {
 
   items = [
       { name: 'Text', icon: 'text_format', onClick: () => this.addBlock.emit(new TextBlock()) },
-      { name: 'Video', icon: 'video_library', onClick: () => this.addBlock.emit(null) },
+      { name: 'Video', icon: 'video_library', onClick: () => this.addBlock.emit(new VideoBlock()) },
       { name: 'Audio', icon: 'audiotrack', onClick: () => this.addBlock.emit(null) },
       { name: 'Image', icon: 'image', onClick: () => this.addBlock.emit(new ImageBlock()) },
       { name: 'Tip', icon: 'priority_high', onClick: () => this.addBlock.emit(new TipBlock()) }
